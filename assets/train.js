08fea2d5acebb051b4da35a8e2de935dbcd93337
rachel doran <rmitchell0925@gmail.com>
2019-08-27 18:30:09 -0400

    modify

--- assets/train.js
+++ assets/train.js
@@ -1,15 +1,6 @@
 $(document).ready(function () {
     console.log("WAZZAP!!");
 
-    // Steps to complete:
-
-    // 1. Initialize Firebase
-    // 2. Create button for adding new employees - then update the html + update the database
-    // 3. Create a way to retrieve employees from the employee database.
-    // 4. Create a way to calculate the months worked. Using difference between start and current time.
-    //    Then use moment.js formatting to set difference in months.
-    // 5. Calculate Total billed
-
     // 1. Initialize Firebase
     var firebaseConfig = {
         apiKey: "AIzaSyBxVc5Lcw22qDj3HLH6y_hWX7efbKUVums",
